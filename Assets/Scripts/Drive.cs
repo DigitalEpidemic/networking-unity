dbd01c8f444c73d9cdb6b822d38dca9ea52598f5
Jeff Polasz <jeff_polasz@hotmail.com>
2017-12-30 00:20:40 -0500

    Setup local player, environment and imports

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Drive : MonoBehaviour {
+
+	float speed = 10.0f;
+	float rotationSpeed = 100.0f;
+
+	// Update is called once per frame
+	void Update () {
+		float translation = CrossPlatformInputManager.GetAxis ("Vertical") * speed;
+		float rotation = CrossPlatformInputManager.GetAxis ("Horizontal") * rotationSpeed;
+
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate (0, 0, translation);
+		transform.Rotate (0, rotation, 0);
+	}
+}
