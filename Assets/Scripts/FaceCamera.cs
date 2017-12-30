cc5b8abcdf764b6618203b7eca3838e3c95dcbc4
Jeff Polasz <jeff_polasz@hotmail.com>
2017-12-30 00:49:37 -0500

    Added player name

---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt (Camera.main.transform.position);
+		this.transform.Rotate (new Vector3 (0, 180, 0));
+	}
+}
