59f810dec77a42ca320609cab1087f4ae0d9219e
Jeff Polasz <jeff_polasz@hotmail.com>
2017-12-30 00:33:32 -0500

    Fixed controls (Only controls local player)

---
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GetComponent<Drive> ().enabled = true;
+		}
+	}
+}
